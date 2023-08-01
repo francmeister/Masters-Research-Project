e8393f87c24142c29ae4e37822dda29ea626a154
francmeister <francophiri97@gmail.com>
2023-08-01 10:29:52 +0200

    01-08-2023

--- Source-Code/Network_Env/SBS.py
+++ Source-Code/Network_Env/SBS.py
@@ -152,15 +152,25 @@ class SBS():
         eMBB_User_energy_consumption = 0
         eMBB_User_channel_rate = 0
         eMBB_User_QOS_requirement_revenue_or_penelaty = 0
+        total_energy = 0
+        total_rate = 0
+        total_QOS_revenue = 0
         self.individual_rewards.clear()
         for eMBB_User in eMBB_Users:
             eMBB_User_energy_consumption = eMBB_User.achieved_total_energy_consumption 
+            total_energy += eMBB_User_energy_consumption
             eMBB_User_channel_rate = eMBB_User.achieved_channel_rate
+            total_rate += eMBB_User_channel_rate
             eMBB_User_QOS_requirement_revenue_or_penelaty = self.achieved_eMBB_delay_requirement_revenue_or_penalty(eMBB_User)
+            total_QOS_revenue += eMBB_User_QOS_requirement_revenue_or_penelaty
             individual_reward = -eMBB_User_energy_consumption + eMBB_User_channel_rate + eMBB_User_QOS_requirement_revenue_or_penelaty
             self.achieved_system_reward += individual_reward
             self.individual_rewards.append(individual_reward)
 
+        print("total_energy: ", total_energy)
+        print("total_rate: ", total_rate)
+        print("total_QOS_revenue: ", total_QOS_revenue)
+        
         if self.num_arriving_URLLC_packets > 0:
             self.achieved_system_reward += ((self.achieved_total_rate_URLLC_users-URLLC_Users[0].QOS_requirement_for_transmission.max_allowable_reliability)/self.num_arriving_URLLC_packets)
 
