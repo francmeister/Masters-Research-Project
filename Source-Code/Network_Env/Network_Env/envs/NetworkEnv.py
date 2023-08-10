b3312e016d36c39c591e5cd7d0b7cf446cefa267
francmeister <francophiri97@gmail.com>
2023-08-10 11:45:05 +0200

    10-08-2023

--- Source-Code/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Source-Code/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -56,17 +56,17 @@ class NetworkEnv(gym.Env):
 
         #Define upper and lower bounds of observation and action spaces
         
-        '''action_space_high = np.array([[self.max_offload_decision for _ in range(self.number_of_users)], [self.num_allocate_subcarriers_upper_bound for _ in range(self.number_of_users)], 
-                        [self.max_transmit_power_db for _ in range(self.number_of_users)], [self.max_number_of_URLLC_users_per_RB for _ in range(self.number_of_users)]], dtype=np.float32)'''
+        action_space_high = np.array([[self.max_offload_decision for _ in range(self.number_of_users)], [self.num_allocate_subcarriers_upper_bound for _ in range(self.number_of_users)], 
+                        [self.max_transmit_power_db for _ in range(self.number_of_users)], [self.max_number_of_URLLC_users_per_RB for _ in range(self.number_of_users)]], dtype=np.float32)
         
-        action_space_high = np.array([[1 for _ in range(self.number_of_users)], [1 for _ in range(self.number_of_users)], 
-                        [1 for _ in range(self.number_of_users)], [1 for _ in range(self.number_of_users)]], dtype=np.float32)
+        '''action_space_high = np.array([[1 for _ in range(self.number_of_users)], [1 for _ in range(self.number_of_users)], 
+                        [1 for _ in range(self.number_of_users)], [1 for _ in range(self.number_of_users)]], dtype=np.float32)'''
 
-        '''action_space_low = np.array([[self.min_offload_decision for _ in range(self.number_of_users)], [self.num_allocate_subcarriers_lower_bound for _ in range(self.number_of_users)], 
-                        [self.min_transmit_power_db for _ in range(self.number_of_users)], [self.min_number_of_URLLC_users_per_RB for _ in range(self.number_of_users)]],dtype=np.float32)'''
+        action_space_low = np.array([[self.min_offload_decision for _ in range(self.number_of_users)], [self.num_allocate_subcarriers_lower_bound for _ in range(self.number_of_users)], 
+                        [self.min_transmit_power_db for _ in range(self.number_of_users)], [self.min_number_of_URLLC_users_per_RB for _ in range(self.number_of_users)]],dtype=np.float32)
         
-        action_space_low = np.array([[-1 for _ in range(self.number_of_users)], [-1 for _ in range(self.number_of_users)], 
-                        [-1 for _ in range(self.number_of_users)], [-1 for _ in range(self.number_of_users)]],dtype=np.float32)
+        '''action_space_low = np.array([[-1 for _ in range(self.number_of_users)], [-1 for _ in range(self.number_of_users)], 
+                        [-1 for _ in range(self.number_of_users)], [-1 for _ in range(self.number_of_users)]],dtype=np.float32)'''
         
         action_space_high = np.transpose(action_space_high)
         action_space_low = np.transpose(action_space_low)
@@ -100,6 +100,7 @@ class NetworkEnv(gym.Env):
 
     def step(self,action):
         action = np.array(action)
+        print(" ")
         print("Action")
         print(action)
         action = np.transpose(action)
