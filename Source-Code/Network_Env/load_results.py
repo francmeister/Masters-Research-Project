c3def0b0dd5be309342b02ba6fd269c23586765c
francmeister <francophiri97@gmail.com>
2023-08-31 03:21:50 +0200

    31-08-2023

--- Source-Code/Network_Env/load_results.py
+++ Source-Code/Network_Env/load_results.py
@@ -1,6 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a_load = np.load('TD3_NetworkEnv-v0_0.npy')
 
 print(a_load)
 print('fdfdf')
+
+# data to be plotted
+timesteps = a_load[:,0]
+rewards = a_load[:,1]
+energies = a_load[:,2]
+throughputs = a_load[:,3]
+ 
+# plotting
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(timesteps, rewards, color ="red")
+plt.plot(timesteps,energies,color = "blue")
+plt.plot(timesteps,throughputs,color = "green")
+plt.legend(["rewards","energies","throughputs"])
+plt.xlabel("Time Steps")
+plt.ylabel("Normalized Reward, Energy Consumption, Throughput")
+plt.title("Graph showing the evolution of reward, energy consumption and throughput over the timesteps")
+plt.show()
