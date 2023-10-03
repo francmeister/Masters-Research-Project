1d7f9afdef4e642bf9329870eb4e184035cecae5
francmeister <francophiri97@gmail.com>
2023-10-03 08:51:01 +0200

    03-10-2023

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -358,8 +358,6 @@ class NetworkEnv(gym.Env):
         self.eMBB_UE_1 = eMBB_UE(1,100,600)
         self.eMBB_UE_2 = eMBB_UE(2,100,600)
         self.eMBB_UE_3 = eMBB_UE(3,100,600)
-        self.eMBB_UE_4 = eMBB_UE(4,100,600)
-        self.eMBB_UE_5 = eMBB_UE(5,100,600)
 
         #Communication Channel
         self.Communication_Channel_1 = Communication_Channel(self.SBS1.SBS_label)
@@ -378,9 +376,6 @@ class NetworkEnv(gym.Env):
         self.eMBB_Users.append(self.eMBB_UE_1)
         self.eMBB_Users.append(self.eMBB_UE_2)
         self.eMBB_Users.append(self.eMBB_UE_3)
-        self.eMBB_Users.append(self.eMBB_UE_4)
-        self.eMBB_Users.append(self.eMBB_UE_5)
-
 
     def check_timestep(self):
         if self.steps >= self.STEP_LIMIT:
