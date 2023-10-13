38ca15ae8083a377d349f1ebe69eb6dfea9132be
francmeister <francophiri97@gmail.com>
2023-10-13 15:55:10 +0200

    13-10-2023

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -32,7 +32,7 @@ class NetworkEnv(gym.Env):
         self.num_allocate_RB_upper_bound = self.Communication_Channel_1.num_allocate_RBs_upper_bound
         self.num_allocate_RB_lower_bound = self.Communication_Channel_1.num_allocate_RBs_lower_bound
         self.max_transmit_power_db = 75#self.eMBB_UE_1.max_transmission_power_dBm
-        self.min_transmit_power_db = 10
+        self.min_transmit_power_db = 65
         self.offload_decisions_label = 0
         self.allocate_num_RB_label = 1
         self.allocate_transmit_powers_label = 2
