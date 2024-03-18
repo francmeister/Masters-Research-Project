6fdc649080c289ad4a7f2157e4a3525a75a253a6
Francis Phiri <francis.phiri@wits.ac.za>
2024-03-18 10:09:13 +0200

    18-03-2024

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -773,7 +773,7 @@ class NetworkEnv(gym.Env):
         #self.eMBB_Users.append(self.eMBB_UE_9)
 
         self.URLLC_Users.append(self.URLLC_UE_1)
-        self.URLLC_Users.append(self.URLLC_UE_2)
+        #self.URLLC_Users.append(self.URLLC_UE_2)
         #self.URLLC_Users.append(self.URLLC_UE_3)
         #self.URLLC_Users.append(self.URLLC_UE_4)
         #self.URLLC_Users.append(self.URLLC_UE_5)
