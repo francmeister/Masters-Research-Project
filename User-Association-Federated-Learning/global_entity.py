3f925054c58553dc14cf807e886e4b04052c1771
Francis Phiri <francis.phiri@wits.ac.za>
2024-04-21 19:42:09 +0200

    21-04-2024

--- User-Association-Federated-Learning/global_entity.py
+++ User-Association-Federated-Learning/global_entity.py
@@ -115,8 +115,6 @@ class GLOBAL_ENTITY():
 
     def aggregate_user_associations(self):
         local_associations = np.array(self.local_associations)
-        print('local_associations')
-        print(local_associations)
         local_associations = np.sum(local_associations,axis=0)
         #self.local_associations = local_associations
  
@@ -124,7 +122,10 @@ class GLOBAL_ENTITY():
 
     def clear_local_user_associations(self):
         self.local_associations_reset_count+=1
+        print('local_associations_reset_count')
+        print(self.local_associations_reset_count)
         if len(self.local_associations) > 0 and self.local_associations_reset_count >= self.num_clients:
+            print('cleared memory')
             self.local_associations.clear()
             self.local_associations_reset_count = 0
             
