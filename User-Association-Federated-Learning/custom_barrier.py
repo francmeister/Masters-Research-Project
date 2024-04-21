0c7f32a4bb5826befd4e91224dee4b30fd6b27c3
Francis Phiri <francis.phiri@wits.ac.za>
2024-04-21 14:19:57 +0200

    21-04-2024

--- User-Association-Federated-Learning/custom_barrier.py
+++ User-Association-Federated-Learning/custom_barrier.py
@@ -6,6 +6,7 @@ class CustomBarrier:
         self.num_threads = num_threads
         self.count = 0
         self.condition = threading.Condition()
+        self.local_associations = []
 
     def wait_for_aggregation(self, global_entity, local_model, access_point_number):
         with self.condition:
@@ -33,8 +34,10 @@ class CustomBarrier:
                 # Perform the aggregation here
                 print("Performing reassociations")
                 user_association = global_entity.aggregate_user_associations()
-                print('aggreagated user_association')
-                print(user_association)
+                self.local_associations = np.array(self.local_associations)
+                print('self.local_associations')
+                print(self.local_associations)
+                self.local_associations = []
                 env.SBS.reassociate_users(user_association)
                 #env.SBS.reassociate_users(np.array([1,2,3,3,2,1,2,2,3,1,3,2]))
                 env.SBS.populate_buffer_memory_sample_with_reward(global_entity.global_reward)
@@ -46,6 +49,7 @@ class CustomBarrier:
                 # Wait for aggregation to complete
                 print("Access Point: ", access_point_number, " waiting for reassociations")
                 SBS_association = env.SBS.predict_future_association(access_point_radius)
+                self.local_associations.append(SBS_association)
                 global_entity.acquire_local_user_associations(SBS_association)
                 global_entity.calculate_global_reward(episode_reward)
                 self.condition.wait()
