f37a2c9628b652d6570f1a6e7c68eb7b4653093b
Francis Phiri <francis.phiri@wits.ac.za>
2024-04-27 14:27:44 +0200

    27-04-2024

--- User-Association-Federated-Learning/result.py
+++ User-Association-Federated-Learning/result.py
@@ -2,11 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 global_reward = np.load('global_reward.npy')
-timesteps = np.load('TD3_NetworkEnv-v0_0.npy')
+#timesteps = np.load('TD3_NetworkEnv-v0_0.npy')
 
-print(global_reward)
+print(len(global_reward))
 
-timesteps = len(global_reward)
+timesteps = []#len(global_reward)
+
+x = 0
+for gb in global_reward:
+    timesteps.append(x)
+    x+=1
+
 
 
 plt.plot(timesteps, global_reward, color ="blue")
