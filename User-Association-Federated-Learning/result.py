a25ac8b46f060af78cd41c37d471a602ae97eae5
Francis Phiri <francis.phiri@wits.ac.za>
2024-04-28 16:01:27 +0200

    28-04-2024

--- User-Association-Federated-Learning/result.py
+++ User-Association-Federated-Learning/result.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-global_reward = np.load('global_reward.npy')
+global_reward = np.load('average_reward_in_memory.npy')
 #timesteps = np.load('TD3_NetworkEnv-v0_0.npy')
 
 print(len(global_reward))
