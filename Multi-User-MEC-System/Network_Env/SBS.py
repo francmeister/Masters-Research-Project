e6e3dc6036d9dc545806d2b2aa3a2cadebb35ca6
Francis Phiri <francis.phiri@wits.ac.za>
2024-05-06 20:20:06 +0200

    06-05-2024

--- Multi-User-MEC-System/Network_Env/SBS.py
+++ Multi-User-MEC-System/Network_Env/SBS.py
@@ -235,8 +235,8 @@ class SBS():
         #print("total_rate: ", total_rate)
         #print("total_QOS_revenue: ", total_QOS_revenue)
         #self.achieved_system_reward
-        return self.achieved_system_reward, self.achieved_system_reward, self.energy_rewards,self.throughput_rewards
-        #return self.achieved_system_reward, overall_users_rewards , self.energy_rewards,self.throughput_rewards
+        #return self.achieved_system_reward, self.achieved_system_reward, self.energy_rewards,self.throughput_rewards
+        return self.achieved_system_reward, self.overall_users_rewards , self.energy_rewards,self.throughput_rewards
 
     def achieved_eMBB_delay_requirement_revenue_or_penalty(self,eMBB_User):
         processing_delay_requirement = eMBB_User.QOS_requirement_for_transmission.max_allowable_latency
