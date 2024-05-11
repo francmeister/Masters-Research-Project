348fb7c9cd0593ff6a4523d1aaeafdf1825e31e0
Francis Phiri <francis.phiri@wits.ac.za>
2024-05-11 08:25:18 +0200

    11-05-2024

--- Multi-User-MEC-System/Network_Env/results_multiple_users.py
+++ Multi-User-MEC-System/Network_Env/results_multiple_users.py
@@ -37,9 +37,9 @@ rewards_7_users_smooth = moving_average(rewards_7_users, window_size)
 rewards_9_users_smooth = moving_average(rewards_9_users, window_size)
 
 
-#plt.plot(timesteps_1_users[window_size-1:], rewards_1_users_smooth, color="green", label="1 User")
-#plt.plot(timesteps_3_users[window_size-1:], rewards_3_users_smooth, color="blue", label='3 Users')
-#plt.plot(timesteps_7_users[window_size-1:], rewards_7_users_smooth, color="red", label='7 Users')
+plt.plot(timesteps_1_users[window_size-1:], rewards_1_users_smooth, color="green", label="1 User")
+plt.plot(timesteps_3_users[window_size-1:], rewards_3_users_smooth, color="blue", label='3 Users')
+plt.plot(timesteps_7_users[window_size-1:], rewards_7_users_smooth, color="red", label='7 Users')
 plt.plot(timesteps_9_users[window_size-1:], rewards_9_users_smooth, color="black", label='9 Users')
 
 plt.xlabel("Timestep(t)")
