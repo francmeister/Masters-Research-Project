fac89a1439e1e8c0cad96ee541c7de81006a3d1e
Francis Phiri <francis.phiri@wits.ac.za>
2024-05-22 15:09:20 +0200

    22-05-2024

--- Multi-User-MEC-System/Network_Env/SBS.py
+++ Multi-User-MEC-System/Network_Env/SBS.py
@@ -244,7 +244,7 @@ class SBS():
         #print("total_rate: ", total_rate)
         #print("total_QOS_revenue: ", total_QOS_revenue)
         #self.achieved_system_reward
-        self.achieved_system_reward = self.achieved_system_reward + 1*fairness_index
+        self.achieved_system_reward = self.achieved_system_reward + 2*fairness_index
         #return self.achieved_system_reward, urllc_reliability_reward_normalized, self.energy_rewards,self.throughput_rewards
         #print('self.achieved_system_reward: ', self.achieved_system_reward)
         return self.achieved_system_reward, self.achieved_system_reward, self.energy_rewards,self.throughput_rewards
