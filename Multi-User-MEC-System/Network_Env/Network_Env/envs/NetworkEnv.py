c71133c768c1917d950b8d342300cf1eb8ad36f4
Francis Phiri <francis.phiri@wits.ac.za>
2024-05-30 20:50:31 +0200

    30-05-2024

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -37,7 +37,7 @@ class NetworkEnv(gym.Env):
         self.num_allocate_RB_lower_bound = self.Communication_Channel_1.num_allocate_RBs_lower_bound
         self.time_divisions_per_slot = self.Communication_Channel_1.time_divisions_per_slot
         #self.max_transmit_power_db = 400#self.eMBB_UE_1.max_transmission_power_dBm
-        self.max_transmit_power_db = 30
+        self.max_transmit_power_db = 1000
         self.min_transmit_power_db = 10
         self.offload_decisions_label = 0
         self.allocate_num_RB_label = 4
