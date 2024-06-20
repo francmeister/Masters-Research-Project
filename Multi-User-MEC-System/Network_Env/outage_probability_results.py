364b05f6e9972633c712f7d11f7b2bed46d35e8d
Francis Phiri <francis.phiri@wits.ac.za>
2024-06-20 15:20:15 +0200

    20-06-2024

--- Multi-User-MEC-System/Network_Env/outage_probability_results.py
+++ Multi-User-MEC-System/Network_Env/outage_probability_results.py
@@ -6,6 +6,7 @@ from numpy import interp
 outage_prabability = np.load('outage_probabilties.npy')
 reliability_reward = np.load('urllc_reliability_reward_normalized.npy')
 power_actions = np.load('power_actions.npy')
+urllc_avg_rate = np.load('urllc_avg_rate.npy')
 
 #print(outage_prabability)
 
@@ -34,7 +35,7 @@ reliability_reward_smooth = moving_average(reliability_reward, window_size)
 # plt.plot(timesteps_9_users[window_size-1:], fairnes_index_9_Users_smooth, color="grey", label='9 Users')
 # plt.plot(timesteps_11_users[window_size-1:], fairnes_index_11_Users_smooth, color="black", label='11 Users')
 
-figure, axis = plt.subplots(3,1)
+figure, axis = plt.subplots(4,1)
 
 axis[0].plot(timesteps, outage_prabability)
 axis[0].set_title('Outage Probability')
@@ -44,6 +45,9 @@ axis[1].set_title('Reliability Reward')
 
 axis[2].plot(timesteps, power_actions)
 axis[2].set_title('Power Actions')
+
+axis[3].plot(timesteps, urllc_avg_rate)
+axis[3].set_title('URLLC avg rate')
 
 #plt.xlabel("Timestep(t)")
 #plt.ylabel("Fairness Index")
