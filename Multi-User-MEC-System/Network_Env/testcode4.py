d87c76f9b5697012ebae24e68cc36fbaf3e90d33
Francis Phiri <francis.phiri@wits.ac.za>
2024-08-19 13:40:35 +0200

    19-08-2024

--- Multi-User-MEC-System/Network_Env/testcode4.py
+++ Multi-User-MEC-System/Network_Env/testcode4.py
@@ -13,7 +13,7 @@ from numpy import interp
 env = gym.make('NetworkEnv-v0')
 
 #timesteps = 5
-timesteps = np.arange(0,1000,1)
+timesteps = np.arange(0,70,1)
 rewards = []
 offload_decisions = []
 RB_allocations = []
