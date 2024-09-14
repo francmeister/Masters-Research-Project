a45daebea22600d5099c3edc0f6af72d984fbfdf
Francis Phiri <francis.phiri@wits.ac.za>
2024-09-14 16:19:40 +0200

    14-09-2024

--- Multi-User-MEC-System/Network_Env/SBS.py
+++ Multi-User-MEC-System/Network_Env/SBS.py
@@ -308,8 +308,8 @@ class SBS():
         #print('throughput_log_reward: ', throughput_log_reward)
         fairness_index_normalized = interp(fairness_index,[0,1],[0,1])
         self.q_action = 10**1
-        q_delay = 10**(-3)
-        q_energy = 10**8
+        q_delay = 1#10**(-3)
+        q_energy = 25**9
         q_total_users_battery_energies_reward = 10**4
         q_urllc_reliability_reward = 10**0
         q_total_offload_traffic_reward = 10**0
@@ -319,7 +319,7 @@ class SBS():
         self.individual_channel_rates.append(individual_channel_rates)
         #self.overall_users_reward = total_users_throughput_reward - self.q_action* (total_users_delay_rewards*total_users_energy_reward) + total_users_battery_energies_reward + urllc_reliability_reward + total_offload_traffic_reward#---------
         #print('total_users_throughput_reward: ', total_users_throughput_reward)
-        self.overall_users_reward = (q_delay*total_users_delay_rewards)#q_throughput*total_users_throughput_reward - (q_energy*total_users_energy_reward) #- (q_delay*total_users_delay_rewards) + (q_throughput_log_reward*throughput_log_reward)#(q_delay*total_users_delay_rewards* q_energy*total_users_energy_reward) #+ q_throughput_log_reward*throughput_log_reward #+ q_total_users_battery_energies_reward*total_users_battery_energies_reward + q_urllc_reliability_reward*urllc_reliability_reward + q_total_offload_traffic_reward*total_offload_traffic_reward
+        self.overall_users_reward = - (q_energy*total_users_energy_reward) - (q_delay*total_users_delay_rewards)#q_throughput*total_users_throughput_reward - (q_energy*total_users_energy_reward) #- (q_delay*total_users_delay_rewards) + (q_throughput_log_reward*throughput_log_reward)#(q_delay*total_users_delay_rewards* q_energy*total_users_energy_reward) #+ q_throughput_log_reward*throughput_log_reward #+ q_total_users_battery_energies_reward*total_users_battery_energies_reward + q_urllc_reliability_reward*urllc_reliability_reward + q_total_offload_traffic_reward*total_offload_traffic_reward
         if self.energy_rewards > 0:
             self.energy_efficiency_rewards = self.throughput_rewards/self.energy_rewards
         else:
