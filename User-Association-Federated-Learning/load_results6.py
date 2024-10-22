1fca6aed19020cc84ede0cbfbac57ee214ad3452
Francis Phiri <francis.phiri@wits.ac.za>
2024-10-22 08:48:32 +0200

    22-10-2024

---
+++ User-Association-Federated-Learning/load_results6.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import interp
+import math
+
+global_reward = np.load('global_reward.npy')
+average_reward_in_memory_AP_1 = np.load('average_reward_in_memory_AP_1.npy')
+average_reward_in_memory_AP_2 = np.load('average_reward_in_memory_AP_2.npy')
+average_reward_in_memory_AP_3 = np.load('average_reward_in_memory_AP_3.npy')
+DNN_training_loss_AP_1 = np.load('DNN_training_loss_AP_1.npy')
+DNN_training_loss_AP_2 = np.load('DNN_training_loss_AP_2.npy')
+DNN_training_loss_AP_3 = np.load('DNN_training_loss_AP_3.npy')
+
+timesteps_global_reward = []
+x = 0
+for gb in global_reward:
+    timesteps_global_reward.append(x)
+    x+=1
+
+timesteps_average_reward_in_memory = []
+x = 0
+for gb in average_reward_in_memory_AP_1:
+    timesteps_average_reward_in_memory.append(x)
+    x+=1
+
+timestep_DNN_training_loss = []
+x = 0
+for gb in DNN_training_loss_AP_1:
+    timestep_DNN_training_loss.append(x)
+    x+=1
+
+figure, axis = plt.subplots(3,4)
+
+
+axis[0,0].plot(timesteps_global_reward, global_reward)
+#axis[0,0].plot(timesteps, overall_users_reward)
+axis[0,0].set_title('Sum Utility Value all APs')
+axis[0,0].set_xlabel('Timestep')
+axis[0,0].set_ylabel('Sum Utility Value')
+axis[0,0].grid()
+
+axis[1,0].plot(timesteps_average_reward_in_memory, average_reward_in_memory_AP_1)
+#axis[1,0].plot(timesteps, energies)
+axis[1,0].set_title('Average Utility Value in Training Memory AP 1')
+axis[1,0].set_xlabel('Timestep')
+axis[1,0].set_ylabel('Average Utility Value')
+axis[1,0].grid()
+
+axis[2,0].plot(timesteps_average_reward_in_memory, average_reward_in_memory_AP_2)
+#axis[1,0].plot(timesteps, energies)
+axis[2,0].set_title('Average Utility Value in Training Memory AP 2')
+axis[2,0].set_xlabel('Timestep')
+axis[2,0].set_ylabel('Average Utility Value')
+axis[2,0].grid()
+
+axis[0,1].plot(timesteps_average_reward_in_memory, average_reward_in_memory_AP_3)
+#axis[1,0].plot(timesteps, energies)
+axis[0,1].set_title('Average Utility Value in Training Memory AP 3')
+axis[0,1].set_xlabel('Timestep')
+axis[0,1].set_ylabel('Average Utility Value')
+axis[0,1].grid()
+
+axis[1,1].plot(DNN_training_loss_AP_1, DNN_training_loss_AP_1)
+#axis[0,1].plot(timesteps, throughputs)
+axis[1,1].set_title('DNN Training Loss AP 1')
+axis[1,1].set_xlabel('Timestep')
+axis[1,1].set_ylabel('Training Loss')
+axis[1,1].grid()
+
+axis[2,1].plot(DNN_training_loss_AP_1, DNN_training_loss_AP_2)
+#axis20,1].plot(timesteps, throughputs)
+axis[2,1].set_title('DNN Training Loss AP 2')
+axis[2,1].set_xlabel('Timestep')
+axis[2,1].set_ylabel('Training Loss')
+axis[2,1].grid()
+
+axis[0,2].plot(DNN_training_loss_AP_1, DNN_training_loss_AP_2)
+#axis[0,1].plot(timesteps, throughputs)
+axis[0,2].set_title('DNN Training Loss AP 2')
+axis[0,2].set_xlabel('Timestep')
+axis[0,2].set_ylabel('Training Loss')
+axis[0,2].grid()
+
+
+# axis[0].plot(evaluation_timesteps1, energy_efficiency_rewards)
+# axis[0].set_title('Energy Efficiency')
+
+
+# axis[1].plot(evaluation_timesteps1[window_size-1:], TD3_smooth)
+# axis[1].set_title('Energy Efficiency')
+
+#plt.plot(timesteps_TD3[window_size-1:], TD3_smooth, color="blue", label='TD3')
+
+plt.tight_layout()
+
+plt.show()
+#plt.figure(1)
+###plt.subplot(211)
+#plt.plot(timesteps, fairness_index, color ="red")
+#plt.subplot(212)
+#plt.plot(timesteps, throughputs, color ="green")
+#plt.subplot(214)
+#plt.plot(timesteps, rewards, color ="blue")
+
+#plt.legend(["reward"])
+#plt.xlabel("Episodes")
+#plt.ylabel("reward")
+#plt.title("Evolution of Reward")
+
+
