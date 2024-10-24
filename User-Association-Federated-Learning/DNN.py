b5a2e0ac78d987991926bf9edddffdd2d506d510
Francis Phiri <francis.phiri@wits.ac.za>
2024-10-24 21:04:51 +0200

    24-10-2024

--- User-Association-Federated-Learning/DNN.py
+++ User-Association-Federated-Learning/DNN.py
@@ -10,10 +10,10 @@ import numpy as np
 class DNN(nn.Module):
     def __init__(self, input_dim, output_dim):
          super(DNN, self).__init__()
-         self.fc1 = nn.Linear(input_dim, 400)
+         self.fc1 = nn.Linear(input_dim, 40)
          self.relu = nn.ReLU()
-         self.fc2 = nn.Linear(400, 300)
-         self.fc3 = nn.Linear(300, output_dim)
+         self.fc2 = nn.Linear(40, 30)
+         self.fc3 = nn.Linear(30, output_dim)
          #self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
