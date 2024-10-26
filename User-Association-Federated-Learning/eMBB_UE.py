83c599372f3a3a456ccc86eeebeb210122f05b50
Francis Phiri <francis.phiri@wits.ac.za>
2024-10-26 13:05:28 +0200

    26-10--2024

--- User-Association-Federated-Learning/eMBB_UE.py
+++ User-Association-Federated-Learning/eMBB_UE.py
@@ -156,7 +156,8 @@ class eMBB_UE(User_Equipment):
             number_of_access_points = len(self.distances_from_access_point)
             number_of_slot = len(self.ap_slot_channel_rates)/number_of_access_points
             print('eMBB user: ', self.eMBB_UE_label, 'number_of_access_points: ', number_of_access_points, 'number_of_slot: ', number_of_slot, 'len(self.ap_slot_channel_rates): ',len(self.ap_slot_channel_rates))
-            #self.ap_slot_channel_rates = np.array(self.ap_slot_channel_rates)
+            self.ap_slot_channel_rates = np.array(self.ap_slot_channel_rates)
+            print('eMBB user: ', self.eMBB_UE_label, 'np array self.ap_slot_channel_rates: ', self.ap_slot_channel_rates)
             self.ap_slot_channel_rates = self.ap_slot_channel_rates.reshape(number_of_access_points,int(number_of_slot))
             print('eMBB user: ', self.eMBB_UE_label, 'slot channel rates reshaped: ', self.ap_slot_channel_rates)
             average_channel_rates = np.mean(self.ap_slot_channel_rates, axis=0)
