90bc281f9be45ac57f0fc6e36cd40310662f923b
Francis Phiri <francis.phiri@wits.ac.za>
2024-11-28 10:59:06 +0200

    28-11-2024

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -150,7 +150,7 @@ class NetworkEnv(gym.Env):
         self.action_space_high = 1
         self.action_space_low = 0
 
-        self.STEP_LIMIT = 32
+        self.STEP_LIMIT = 128
         self.sleep = 0
         self.steps = 0
         self.initial_RB_bandwidth = self.Communication_Channel_1.RB_bandwidth_Hz
