2a25be4ecda20d8e8d94fb2c3684e1d9898c2ba4
Francis Phiri <francis.phiri@wits.ac.za>
2024-12-07 15:30:12 +0200

    07-12-2024

--- Multi-User-MEC-System/Network_Env/TD3_multiple_users_inference_FI.py
+++ Multi-User-MEC-System/Network_Env/TD3_multiple_users_inference_FI.py
@@ -3,22 +3,25 @@ import matplotlib.pyplot as plt
 from numpy import interp
 
 
-TD3_energy_multiple_users = [0.067788,0.161190,0.254607]
+TD3_energy_multiple_users_10_6_FI = [0.067788,0.161190,0.254607]
+TD3_energy_multiple_users_20_6_FI = [0.149311,]
 DDPG_energy_multiple_users = []
 full_offloading_multiple_users_energy = [0.00016913605774023597,0.0001703231941821021,0.000172211157922183]#3,7,11 users
 local_computing_multiple_users_energy = [0.19027157569099212,0.44367963230784163,0.6970266853942844]
 
-TD3_delay_multiple_users = [3.198654,7.425685,11.754907]
+TD3_delay_multiple_users_10_6_FI = [3.198654,7.425685,11.754907]
+TD3_delay_multiple_users_10_6_FI = [174.002204]
 DDPG_delay_multiple_users = []
 full_offloading_multiple_users_delay = [3.0001200707379643,7.0099352994397455,11.180665901630894]
 local_computing_multiple_users_delay = [315.4792111021463,735.0747981843205,1158.8974912077504]
 
-TD3_throughput_multiple_users = [36181414.966455,31511069.986704,34897126.303756]
+TD3_throughput_multiple_users_10_6_FI = [36181414.966455,31511069.986704,34897126.303756]
+TD3_throughput_multiple_users_20_6_FI = [23424058.725300,]
 DDPG_throughput_multiple_users = []
 full_offloading_multiple_users_throughput = [33978835.375478804,31914810.185659975,28852512.997813337] # with random resource allocation and power allocation
 
-TD3_fairness_index_multiple_users_10_6 = [0.710785,0.654622,0.525979]
-#TD3_fairness_index_multiple_users_20_6 = [0.710785,0.654622,0.525979]
+TD3_fairness_index_multiple_users_10_6_FI = [0.710785,0.654622,0.525979]
+TD3_fairness_index_multiple_users_20_6 = [0.882637]
 DDPG_fairness_index_multiple_users = []
 random_resource_allocation_fairness_index_multiple_users = [0.8534970089224784,0.6627020041731907,0.5150579831673366]
 
