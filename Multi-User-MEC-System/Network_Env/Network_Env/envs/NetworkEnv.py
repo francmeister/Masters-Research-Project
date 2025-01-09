f76026eae5174fcf0adfd7032b801fa490d21715
Francis Phiri <francis.phiri@wits.ac.za>
2025-01-09 13:53:22 +0200

    09-01-2025

--- Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
+++ Multi-User-MEC-System/Network_Env/Network_Env/envs/NetworkEnv.py
@@ -660,7 +660,7 @@ class NetworkEnv(gym.Env):
         #collect the final action - number of URLLC users per RB
         
         #print('Action after interpolation transposed')
-        offload_decisions_actions_mapped = [0,0,0,0,0,0,0,0,0,0,0]#[0, 0, 0.5, 0.5, 1, 1, 1]
+        #offload_decisions_actions_mapped = [0,0,0,0,0,0,0,0,0,0,0]#[0, 0, 0.5, 0.5, 1, 1, 1]
         #transmit_power_actions_mapped = [20,20,20,20,20,20,20,20,20,20,20]#,20,20,20,20,20,20]
         #RB_allocation_actions = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])#, [0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0]])
         #print('RB_allocation_actions: ', RB_allocation_actions)
@@ -1111,16 +1111,16 @@ class NetworkEnv(gym.Env):
     def group_users(self):
         #Group all eMBB Users
         self.eMBB_Users.append(self.eMBB_UE_1)
-        #self.eMBB_Users.append(self.eMBB_UE_2)
-        #self.eMBB_Users.append(self.eMBB_UE_3)
-        #self.eMBB_Users.append(self.eMBB_UE_4)
-        #self.eMBB_Users.append(self.eMBB_UE_5)
-        #self.eMBB_Users.append(self.eMBB_UE_6)
-        #self.eMBB_Users.append(self.eMBB_UE_7)
-        #self.eMBB_Users.append(self.eMBB_UE_8)
-        #self.eMBB_Users.append(self.eMBB_UE_9)
-        #self.eMBB_Users.append(self.eMBB_UE_10)
-        #self.eMBB_Users.append(self.eMBB_UE_11)
+        self.eMBB_Users.append(self.eMBB_UE_2)
+        self.eMBB_Users.append(self.eMBB_UE_3)
+        self.eMBB_Users.append(self.eMBB_UE_4)
+        self.eMBB_Users.append(self.eMBB_UE_5)
+        self.eMBB_Users.append(self.eMBB_UE_6)
+        self.eMBB_Users.append(self.eMBB_UE_7)
+        self.eMBB_Users.append(self.eMBB_UE_8)
+        self.eMBB_Users.append(self.eMBB_UE_9)
+        self.eMBB_Users.append(self.eMBB_UE_10)
+        self.eMBB_Users.append(self.eMBB_UE_11)
 
         #self.URLLC_Users.append(self.URLLC_UE_1)
         #self.URLLC_Users.append(self.URLLC_UE_2)
